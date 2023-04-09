b7641d0d8deacbe053d5b3abbfb2c7d09104c4ba
luanrivello <luanrivello@hotmail.com>
2023-04-09 09:56:14 -0300

    Create log basic structure

--- eventlog/akashicrecords.go
+++ eventlog/akashicrecords.go
@@ -1,1 +1,23 @@
 package eventlog
+
+import (
+	"conquest/creatures"
+	"conquest/spacetime"
+)
+
+type AkashicRecords struct {
+	actions map[string]ActionLog
+	events  map[string]EventLog
+}
+
+type ActionLog struct {
+	actor    creatures.Creature
+	action   string
+	target   creatures.Creature
+	location spacetime.PlaceInterface
+}
+
+type EventLog struct {
+	event    string
+	location spacetime.PlaceInterface
+}
