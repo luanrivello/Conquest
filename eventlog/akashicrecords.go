178b85ae015c66b4d79492231a1a98573d178191
luanrivello <luanrivello@hotmail.com>
2023-04-21 21:35:42 -0300

    Add pointer to creature

--- eventlog/akashicrecords.go
+++ eventlog/akashicrecords.go
@@ -17,7 +17,18 @@ type logInterface interface {
 	String() string
 }
 
-func (records *AkashicRecords) AddAction(actor creatures.Creature, action string, location st.Planet, time int) {
+type actionLog struct {
+	Actor    *creatures.Creature
+	Action   string
+	Location *st.Planet
+	Time     int
+}
+
+func (log actionLog) String() string {
+	return fmt.Sprintf("[time: %04d] %s %s at %s", log.Time, log.Actor.String(), log.Action, log.Location.GetName())
+}
+
+func (records *AkashicRecords) AddAction(actor *creatures.Creature, action string, location *st.Planet, time int) {
 	entry := actionLog{
 		Actor:    actor,
 		Action:   action,
@@ -25,17 +36,6 @@ func (records *AkashicRecords) AddAction(actor creatures.Creature, action string
 		Time:     time,
 	}
 	records.Actions = append(records.Actions, entry)
-}
-
-type actionLog struct {
-	Actor    creatures.Creature
-	Action   string
-	Location st.Planet
-	Time     int
-}
-
-func (log actionLog) String() string {
-	return fmt.Sprintf("[time: %04d] %s %s at %s", log.Time, log.Actor.String(), log.Action, log.Location.GetName())
 }
 
 type actionToTargetLog struct {
